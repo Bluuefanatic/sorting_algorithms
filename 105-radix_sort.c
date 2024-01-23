378d7d23077cbc5315edba8c46e7895d5fc76781
Bluuefanatic <izirenjoel@gmail.com>
2024-01-23 15:21:20 -0800

    Implement Radix sort algorithm using LSD for sorting array

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,47 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * radix_sort - Sorts an array of integers in ascend order using LSD Radix sort
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
 void print_array(const int *array, size_t size);
 
 void radix_sort(int *array, size_t size)
 {
-    size_t i;
-    int exp;
-    int max;
+	size_t i;
+	int exp;
+	int max;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    max = array[0];
+	max = array[0];
 
-    for (i = 1; i < size; ++i)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
+	for (i = 1; i < size; ++i)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
 
-    for (exp = 1; max / exp > 0; exp *= 10)
-    {
-        int count[10] = {0};
-        ssize_t j;
-        int *output = malloc(sizeof(int) * size);
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		int count[10] = {0};
+		ssize_t j;
+		int *output = malloc(sizeof(int) * size);
 
-        for (i = 0; i < size; ++i)
-            count[(array[i] / exp) % 10]++;
+		for (i = 0; i < size; ++i)
+			count[(array[i] / exp) % 10]++;
 
-        for (i = 1; i < 10; ++i)
-            count[i] += count[i - 1];
+		for (i = 1; i < 10; ++i)
+			count[i] += count[i - 1];
 
-        for (j = size - 1; j >= 0; --j)
-        {
-            output[count[(array[j] / exp) % 10] - 1] = array[j];
-            count[(array[j] / exp) % 10]--;
-        }
+		for (j = size - 1; j >= 0; --j)
+		{
+			output[count[(array[j] / exp) % 10] - 1] = array[j];
+			count[(array[j] / exp) % 10]--;
+		}
 
-        for (i = 0; i < size; ++i)
-            array[i] = output[i];
+		for (i = 0; i < size; ++i)
+			array[i] = output[i];
 
-        print_array(array, size);
-        free(output);
-    }
+		print_array(array, size);
+		free(output);
+	}
 }
