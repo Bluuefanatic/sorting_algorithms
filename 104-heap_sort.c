64510e0522de4e8964a0de965428257268bb3656
Bluuefanatic <izirenjoel@gmail.com>
2024-01-23 14:49:37 -0800

    Implement Heap sort algorithm with sift-down operation for sorting array

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -9,27 +9,27 @@
  */
 void sift_down(int *array, size_t size, size_t root, size_t end)
 {
-    size_t max, left_child, right_child;
+	size_t max, left_child, right_child;
 
-    max = root;
-    left_child = 2 * root + 1;
-    right_child = 2 * root + 2;
+	max = root;
+	left_child = 2 * root + 1;
+	right_child = 2 * root + 2;
 
-    if (left_child < end && array[left_child] > array[max])
-        max = left_child;
+	if (left_child < end && array[left_child] > array[max])
+		max = left_child;
 
-    if (right_child < end && array[right_child] > array[max])
-        max = right_child;
+	if (right_child < end && array[right_child] > array[max])
+		max = right_child;
 
-    if (max != root)
-    {
-        /* Swap root and max */
-        array[root] ^= array[max];
-        array[max] ^= array[root];
-        array[root] ^= array[max];
-        print_array(array, size);
-        sift_down(array, size, max, end);
-    }
+	if (max != root)
+	{
+		/* Swap root and max */
+		array[root] ^= array[max];
+		array[max] ^= array[root];
+		array[root] ^= array[max];
+		print_array(array, size);
+		sift_down(array, size, max, end);
+	}
 }
 
 /**
@@ -39,19 +39,19 @@ void sift_down(int *array, size_t size, size_t root, size_t end)
  */
 void heap_sort(int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    /* Build max heap */
-    for (i = size / 2; i > 0; i--)
-        sift_down(array, size, i - 1, size);
+	/* Build max heap */
+	for (i = size / 2; i > 0; i--)
+		sift_down(array, size, i - 1, size);
 
-    for (i = size - 1; i > 0; i--)
-    {
-        /* Swap root and last element */
-        array[0] ^= array[i];
-        array[i] ^= array[0];
-        array[0] ^= array[i];
-        print_array(array, size);
-        sift_down(array, size, 0, i);
-    }
+	for (i = size - 1; i > 0; i--)
+	{
+		/* Swap root and last element */
+		array[0] ^= array[i];
+		array[i] ^= array[0];
+		array[0] ^= array[i];
+		print_array(array, size);
+		sift_down(array, size, 0, i);
+	}
 }
